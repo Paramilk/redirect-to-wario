2d779dfb7c0fde213e3e1a44eaeccbc349d5e337
Paramilk <82511910+Paramilk@users.noreply.github.com>
2021-11-05 14:36:30 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open("https://www.google.com/search?q=wario&client=ubuntu&hs=xdO&channel=fs&sxsrf=AOaemvI1N_Wdkq_9uZNaZhTvQ1dRChJz-g:1636122462504&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjcpb6Rt4H0AhWEgVwKHdjCD1AQ_AUoAXoECAEQAw")
